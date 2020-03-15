3c049343c4ba31a66ca3f6577b109b6eba46d679
Romena Murgor <romenamurgor@gmail.com>
2020-03-15 05:01:13 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -1,14 +1,12 @@
-describe "smallest hash value" do
-  it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-  end
+def key_for_min_value(name_hash)
 
-  it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
+ low = nil
+ low_name = nil
+ name_hash.each do |key, value|
+    if low == nil || low > value
+      low = value
+      low_name = key
+    end
   end
-
-  it "returns nil for an empty hash" do
-    expect(key_for_min_value({})).to eq(nil)
-  end
-
-end
+  low_name
+end 
